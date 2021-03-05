bfd48bd34856e7470af09e95add845b313a1e302
khairilazran <46481050+khairilazran@users.noreply.github.com>
2021-03-05 16:15:41 +0800

    Base code 1

---
+++ src/generate.py
@@ -0,0 +1,16 @@
+# this program reads logged sensor value to determine if a wifi-access code should be generated
+
+# notes:
+# might want to generate code as a QR code using (pip install qrcode[pil])
+
+# change "Output.txt" at whatever point
+sensor_file = open("Output.txt", "r")
+# cast string to int, hopefully
+sensor_value = int(sensor_file.read())
+
+if sensor_value == 1:
+    print("Purchase authenticated!\nDispensing WiFi Access code")
+else:
+    print("Purchase invalid!")
+
+sensor_file.close()
