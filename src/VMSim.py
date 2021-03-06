660e0addca1846ab99dc91ef25b77c5dbc8bd2f3
khairilazran <46481050+khairilazran@users.noreply.github.com>
2021-03-06 15:19:02 +0800

    Title

--- src/VMSim.py
+++ src/VMSim.py
@@ -1,6 +1,4 @@
 import serial
-from tkinter import *
-import os 
 
 port = "COM3"
 ser = serial.Serial(port,9600)
@@ -8,8 +6,3 @@ value = 0
 
 if(ser.read()):
 	print("Success!")
-  
-    #if value == "1":
-#print(ser.name)
-       	#os.startfile("qrcode.py")
-       	
