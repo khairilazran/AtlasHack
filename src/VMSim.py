51ed1bdcd1b0a6c56da0ac05bdaf577389266b13
oscarroo <66630991+oscarroo@users.noreply.github.com>
2021-03-06 15:21:58 +0800

    Update VMSim.py

--- src/VMSim.py
+++ src/VMSim.py
@@ -1,6 +1,6 @@
 import serial
 
-ser = serial.Serial("COM3,9600) //setup the serial comms
+ser = serial.Serial("COM3,9600) #setup the serial comms
 
-if(ser.read()): //if read input from COM3, do something...
+if(ser.read()): #if read input from COM3, do something...
 	print("Success!")
